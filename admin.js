9a71f542a4aa038f75c42950e5a68eed0a9adaff
Joel Aliyu <joelaliyu1@gmail.com>
2024-07-10 14:22:31 +0100

    Implemented the sorting and filtering in the admin view

--- admin.js
+++ admin.js
@@ -1,31 +1,93 @@
 $(document).ready(function() {
+    let patientData = [];
+
     function fetchPatientDetails() {
         $.ajax({
             url: 'index.php?action=getPatientDetails',
             type: 'GET',
             dataType: 'json',
             success: function(data) {
-                var tableBody = $('#patientDetails tbody');
-                tableBody.empty();
-                $.each(data.data, function(index, patient) {
-                    var $row = $('<tr>').append(
-                        $('<td>').text(patient.SubmissionDate),
-                        $('<td>').text(patient.FirstName),
-                        $('<td>').text(patient.Surname),
-                        $('<td>').text(patient.Age),
-                        $('<td>').text(patient.DateOfBirth),
-                        $('<td>').text(patient.TotalScore)
-                    );
-                    $row.data('patient-id', patient.ID);
-                    tableBody.append($row);
-                });
+                patientData = data.data;
+                renderTable(patientData);
             },
             error: function(xhr, status, error) {
-                $('#error-message').text(error);
+                $('#error-message').text(error).removeClass('d-none');
             }
         });
     }
-    fetchPatientDetails();
+
+    function renderTable(data) {
+        var tableBody = $('#patientDetails tbody');
+        tableBody.empty();
+        $.each(data, function(index, patient) {
+            var $row = $('<tr>').append(
+                $('<td>').text(patient.SubmissionDate),
+                $('<td>').text(patient.FirstName),
+                $('<td>').text(patient.Surname),
+                $('<td>').text(patient.Age),
+                $('<td>').text(patient.DateOfBirth),
+                $('<td>').text(patient.TotalScore)
+            );
+            $row.data('patient-id', patient.ID);
+            tableBody.append($row);
+        });
+    }
+
+    $('#filterByFirstName').on('input', function() {
+        var filterValue = $(this).val().toLowerCase();
+        var filteredData = patientData.filter(function(patient) {
+            return patient.FirstName.toLowerCase().includes(filterValue);
+        });
+        renderTable(filteredData);
+    });
+
+    $('#filterBySurname').on('input', function() {
+        var filterValue = $(this).val().toLowerCase();
+        var filteredData = patientData.filter(function(patient) {
+            return patient.Surname.toLowerCase().includes(filterValue);
+        });
+        renderTable(filteredData);
+    });
+
+    $('#filterByDateOfBirth').on('input', function() {
+        var filterValue = $(this).val();
+        var filteredData = patientData.filter(function(patient) {
+            return patient.DateOfBirth.includes(filterValue);
+        });
+        renderTable(filteredData);
+    });
+
+    $('#clearFilters').click(function() {
+        $('#filterFirstName').val('');
+        $('#filterSurname').val('');
+        $('#filterDateOfBirth').val('');
+        renderTable(patientData);
+    });
+
+    function sortTable(column, order) {
+        var sortedData = patientData.sort(function(a, b) {
+            if (a[column] < b[column]) return order === 'asc' ? -1 : 1;
+            if (a[column] > b[column]) return order === 'asc' ? 1 : -1;
+            return 0;
+        });
+        renderTable(sortedData);
+    }
+
+    $('#sortByFirstName').click(function() {
+        sortTable('FirstName', 'asc');
+    });
+
+    $('#sortBySurname').click(function() {
+        sortTable('Surname', 'asc');
+    });
+
+    $('#sortByAge').click(function() {
+        sortTable('Age', 'asc');
+    });
+
+    $('#sortByTotalScore').click(function() {
+        sortTable('TotalScore', 'asc');
+    });
 
     function getAllPatientDetails(patientId) {
         $.ajax({
@@ -35,20 +97,21 @@ $(document).ready(function() {
             dataType: 'json',
             success: function(response) {
                 if (response.status === 'success') {
-                    $('#firstName').val(response.data.FirstName);
-                    $('#surname').val(response.data.Surname);
-                    $('#treatment').val(response.data.ReliefPainTreatments);
-                    $('#worst').val(response.data.PainWorst);
-                    $('#least').val(response.data.PainLeast);
-                    $('#average').val(response.data.PainAverage);
-                    $('#now').val(response.data.PainRightNow);
-                    $('#activity').val(response.data.PainInterferedGeneralActivity);
-                    $('#mood').val(response.data.PainInterferedMood);
-                    $('#walking').val(response.data.PainInterferedWalkingAbility);
-                    $('#work').val(response.data.PainInterferedNormalWork);
-                    $('#relationships').val(response.data.PainInterferedRelationships);
-                    $('#sleep').val(response.data.PainInterferedSleep);
-                    $('#enjoyment').val(response.data.PainInterferedEnjoymentOfLife);
+                    var data = response.data;
+                    $('#firstName').val(data.FirstName);
+                    $('#surname').val(data.Surname);
+                    $('#treatment').val(data.ReliefPainTreatments);
+                    $('#worst').val(data.PainWorst);
+                    $('#least').val(data.PainLeast);
+                    $('#average').val(data.PainAverage);
+                    $('#now').val(data.PainRightNow);
+                    $('#activity').val(data.PainInterferedGeneralActivity);
+                    $('#mood').val(data.PainInterferedMood);
+                    $('#walking').val(data.PainInterferedWalkingAbility);
+                    $('#work').val(data.PainInterferedNormalWork);
+                    $('#relationships').val(data.PainInterferedRelationships);
+                    $('#sleep').val(data.PainInterferedSleep);
+                    $('#enjoyment').val(data.PainInterferedEnjoymentOfLife);
 
                     $('#patientForm').data('patient-id', patientId);
                     $('#patientDetailsForm').removeClass('d-none').show();
@@ -64,11 +127,13 @@ $(document).ready(function() {
         getAllPatientDetails(patientId);
     });
 
+    // EDIT
     $('#editBtn').click(function() {
         $('input').removeAttr('readonly');
         $('#updateBtn').removeClass('d-none');
     });
 
+    // UPDATE
     $('#updateBtn').click(function() {
         var patientId = $('#patientForm').data('patient-id');
         var updatedDetails = {
@@ -110,6 +175,7 @@ $(document).ready(function() {
         });
     });
 
+    // DELETE
     $('#deleteBtn').click(function() {
         var patientId = $('#patientForm').data('patient-id');
         if (confirm("Are you sure you want to delete this patient's record?")) {
@@ -117,19 +183,21 @@ $(document).ready(function() {
                 url: 'index.php',
                 type: 'POST',
                 data: JSON.stringify({action: 'deletePatient', patientId: patientId}),
-                contentType: 'application/json',
                 dataType: 'json',
+                contentType: 'application/json',
                 success: function(response) {
                     if (response.status === 'success') {
                         fetchPatientDetails();
-                        $('#success-msg').text("Patient's record deleted successfully").removeClass('d-none').delay(4000).fadeOut();
+                        $('#success-msg').text("Patient's record deleted successfully").removeClass('d-none').delay(4000).fadeOut(2000);
                         $('#patientDetailsForm').hide();
                     } else {
                         fetchPatientDetails();
-                        $('#error-msg').text("Unable to delete patient's record");
+                        $('#error-msg').text("Unable to delete patient's record").removeClass('d-none');
                     }
                 }
             });
         }
     });
+
+    fetchPatientDetails();
 });
